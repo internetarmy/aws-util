63c297a5f5bd8faa5fbde82fd37440945ca84e35
sivasankar.mullu <shakesiva143@gmail.com>
2024-01-13 23:14:17 +0530

    fixed an query issue.

--- src/main/java/com/internetarmy/aws/repo/EmployeeRepo.java
+++ src/main/java/com/internetarmy/aws/repo/EmployeeRepo.java
@@ -25,10 +25,10 @@ public class EmployeeRepo {
 		StringBuilder builder = new StringBuilder();
 		builder.append("select * from employee where 1=1 ");
 		if(id != null) {
-			builder.append("and id = :id");
+			builder.append("and id = :id ");
 		}
 		if(name != null && !name.isBlank()) {
-			builder.append("and employee_name = :name");
+			builder.append("and employee_name = :name ");
 		}
 		Query query = em.createNativeQuery(builder.toString(), Employee.class);
 		if(id != null) query.setParameter("id", id);
